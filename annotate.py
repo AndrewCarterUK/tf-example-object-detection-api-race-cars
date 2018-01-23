a4509820f9db30b89173a80c18722ff19c733ee2
Andrew Carter <andrew.carter@smarterdm.com>
2018-01-23 23:58:27 +0000

    Test data and annotation generator

---
+++ annotate.py
@@ -0,0 +1,95 @@
+import numpy as np
+import os
+import tensorflow as tf
+from PIL import Image
+from utils import label_map_util
+from utils import visualization_utils as vis_util
+from pascal_voc_writer import Writer
+
+if tf.__version__ < '1.4.0':
+  raise ImportError('Please upgrade your tensorflow installation to v1.4.* or later!')
+
+PATH_TO_CKPT = 'inference/frozen_inference_graph.pb'
+PATH_TO_LABELS = 'data/map.pbtxt'
+
+NUM_CLASSES = 1
+
+detection_graph = tf.Graph()
+with detection_graph.as_default():
+  od_graph_def = tf.GraphDef()
+  with tf.gfile.GFile(PATH_TO_CKPT, 'rb') as fid:
+    serialized_graph = fid.read()
+    od_graph_def.ParseFromString(serialized_graph)
+    tf.import_graph_def(od_graph_def, name='')
+
+label_map = label_map_util.load_labelmap(PATH_TO_LABELS)
+categories = label_map_util.convert_label_map_to_categories(label_map, max_num_classes=NUM_CLASSES, use_display_name=True)
+category_index = label_map_util.create_category_index(categories)
+
+def load_image_into_numpy_array(image):
+  (im_width, im_height) = image.size
+  return np.array(image.getdata()).reshape(
+      (im_height, im_width, 3)).astype(np.uint8)
+
+PATH_TO_TEST_IMAGES_DIR = 'test'
+TEST_IMAGE_PATHS = [ os.path.join(PATH_TO_TEST_IMAGES_DIR, '{}.jpg'.format(i)) for i in [1,2,3,4] ]
+
+with detection_graph.as_default():
+  with tf.Session(graph=detection_graph) as sess:
+    # Definite input and output Tensors for detection_graph
+    image_tensor = detection_graph.get_tensor_by_name('image_tensor:0')
+    # Each box represents a part of the image where a particular object was detected.
+    detection_boxes = detection_graph.get_tensor_by_name('detection_boxes:0')
+    # Each score represent how level of confidence for each of the objects.
+    # Score is shown on the result image, together with the class label.
+    detection_scores = detection_graph.get_tensor_by_name('detection_scores:0')
+    detection_classes = detection_graph.get_tensor_by_name('detection_classes:0')
+    num_detections = detection_graph.get_tensor_by_name('num_detections:0')
+    for image_path in TEST_IMAGE_PATHS:
+      image = Image.open(image_path)
+      image_width, image_height = image.size
+      # the array based representation of the image will be used later in order to prepare the
+      # result image with boxes and labels on it.
+      image_np = load_image_into_numpy_array(image)
+      # Expand dimensions since the model expects images to have shape: [1, None, None, 3]
+      image_np_expanded = np.expand_dims(image_np, axis=0)
+      # Actual detection.
+      (boxes, scores, classes, num) = sess.run(
+          [detection_boxes, detection_scores, detection_classes, num_detections],
+          feed_dict={image_tensor: image_np_expanded})
+
+      boxes = np.squeeze(boxes)
+      classes = np.squeeze(classes)
+      scores = np.squeeze(scores)
+
+      writer = Writer(image_path, image_width, image_height)
+
+      for index, score in enumerate(scores):
+        if score < 0.5:
+          continue
+
+        label = category_index[classes[index]]['name']
+        ymin, xmin, ymax, xmax = boxes[index]
+
+        writer.addObject(label, int(xmin * image_width), int(ymin * image_height),
+                         int(xmax * image_width), int(ymax * image_height))
+
+
+      annotation_path = os.path.splitext(image_path)[0] + '.xml'
+      writer.save(annotation_path)
+
+      # Visualization of the results of a detection.
+      vis_util.visualize_boxes_and_labels_on_image_array(
+          image_np,
+          boxes,
+          classes.astype(np.int32),
+          scores,
+          category_index,
+          min_score_thresh=.2,
+          use_normalized_coordinates=True,
+          line_thickness=8)
+
+      output_image_path = os.path.splitext(image_path)[0] + '-boxes.jpg'
+      output_image = Image.fromarray(image_np)
+      output_image.save(output_image_path)
+
